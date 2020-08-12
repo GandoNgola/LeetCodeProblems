0bfba580b8443c955f7b8962b6f4249527d4c66a
guvarallo <gustavo.varallo@live.com>
2020-08-12 18:33:49 -0300

    Added count-primes

--- count-primes.js
+++ count-primes.js
@@ -8,24 +8,26 @@
 
 // Runtime 144ms faster than 78.30% JS submissions
 const countPrimes = n => {
-  // Here we use the Sieve of Erastosthenes, where we first assume all numbers to be prime (true).
-  // For this we create an array of n size and then iterate through them setting the non-prime to false.
+  // Here we use the Sieve of Eratosthenes.
+  // For this we create an array of size n with true values.
   let arr = Array(n).fill(true);
   // 0 and 1 are not prime, so we set them to false and start our iteration on i=2.
   arr[0] = false;
   arr[1] = false;
 
+  // Now we'll iterate through the array setting the non-prime to false.
   // If n is not prime, it'll always have a factor that is < or = to its sqrt.
   // Example: Number 36. Its factors are 1*36, 2*18, 3*12, 4*9 and finally 6*6.
   // Each case of 36 factors always contains one number < or = its sqrt.
-  // This way we only need to iterate until i*i < n (in the 36 case, until 5*5 since we don't need the 36 itself)
+  // This way we only need to iterate until i*i < n (taking n as 36, only until 
+  // 5*5 since we don't need the n itself, only the count of primes until n).
   for (let i = 2; i*i < n; i++) {
     if (arr[i]) {
       // The inner loop will iterate over the multiples of i (2 to start with) and set them to false.
       // So first iteration starts at 4: j=i*i(4) => set arr[4] to false => j+=i(4+2=6). 
       // Next iteration will set arr[6] to false... until j < n.
       // Then we return to the outer loop and check the multiples of i++ (3 is the next one)
-      // Once i*i > n, we can stop iterating as explained above.
+      // Once i*i > n, we can stop iterating as explained in the Number 36 example.
       for (j = i*i; j < n; j+=i) {
         arr[j] = false;
       }
