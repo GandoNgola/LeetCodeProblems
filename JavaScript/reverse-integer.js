665e47e45db64c50832105a8d528568fee05c701
guvarallo <gustavo.varallo@live.com>
2020-08-14 19:18:00 -0300

    Removed comments params

--- JavaScript/reverse-integer.js
+++ JavaScript/reverse-integer.js
@@ -16,9 +16,6 @@
 // Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. 
 // For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
 
-// * @param {number} x
-// * @return {number}
-
 // Runtime of 84ms, faster than 90.45% of JS submissions
 const reverse = x => {
   let arr = ('' + Math.abs(x)).split('');
